bdfd3f0189b28353f8451076d979a9854d829414
Anton-4 <anton.vanmoere@gmail.com>
2020-04-02 15:23:48 +0200

    scalatest deprecation fix

--- src/main/g8/src/test/scala/example/MyBaseSpec.scala
+++ src/main/g8/src/test/scala/example/MyBaseSpec.scala
@@ -1,8 +1,10 @@
 package example
 
-import org.scalatest.{BeforeAndAfterAll, FlatSpec, Matchers}
+import org.scalatest.{BeforeAndAfterAll}
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
 import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
 
-trait MyBaseSpec extends FlatSpec with Matchers with BeforeAndAfterAll with ScalaCheckDrivenPropertyChecks {
+trait MyBaseSpec extends AnyFlatSpec with Matchers with BeforeAndAfterAll with ScalaCheckDrivenPropertyChecks {
 
 }
